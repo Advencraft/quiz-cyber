6e0dc88b15484ffdb549169ec339751928bf925d
marie <marie@sio.ndlp>
2025-11-25 17:56:19 +0100

    Amélioration de l'affichage | Afficher_question

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,17 @@
 
 import { useEffect, useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
+// Components
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Spinner } from "@/components/ui/spinner"
+import {
+  Empty,
+  EmptyDescription,
+  EmptyHeader,
+  EmptyMedia,
+  EmptyTitle,
+} from "@/components/ui/empty"
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
 
 export default function Home() {
   const [question, setQuestion] = useState<any>(null);
@@ -19,17 +29,37 @@ export default function Home() {
     fetchQuestion();
   }, []);
 
-  return question ? (
-  <Card className="max-w-xl mx-auto mt-6">
-    <CardHeader>
-      <CardTitle>Question</CardTitle>
-    </CardHeader>
-    <CardContent>
-      <p>{question.texte}</p>
-    </CardContent>
-  </Card>
-) : (
-  <p>Chargement de la question...</p>
+  return (
+  <div>
+    <Alert className="bg-blue-50 border-blue-300 text-blue-800 max-w-xl mx-auto mt-6">
+      <AlertTitle className="text-xl font-semibold">Bienvenue sur CyberQuiz</AlertTitle>
+      <AlertDescription>
+        Préparez-vous à tester vos connaissances !
+      </AlertDescription>
+    </Alert>
+  {question ? (
+    <Card className="max-w-xl mx-auto mt-6">
+      <CardHeader>
+        <CardTitle>Question</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <p>{question.texte}</p>
+      </CardContent>
+    </Card>
+  ) : (
+      <Empty className="w-full">
+        <EmptyHeader>
+          <EmptyMedia variant="icon">
+            <Spinner />
+          </EmptyMedia>
+          <EmptyTitle>Chargement de la question...</EmptyTitle>
+          <EmptyDescription>
+            Le Chargement peut durer un certain temps.
+          </EmptyDescription>
+        </EmptyHeader>
+      </Empty>
+    )}
+  </div>
 );
 
 }
