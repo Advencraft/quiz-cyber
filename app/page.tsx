858208fe600f1b15d362da2a9787cb264aab02a7
MARIE Théo <theomarie50350@gmail.com>
2025-11-29 19:01:01 +0100

    réponse choisi en couleur | progresse bar question | Bouton passé les questions

--- app/page.tsx
+++ app/page.tsx
@@ -10,13 +10,18 @@ import { Spinner } from "@/components/ui/spinner"
 import {Empty,EmptyDescription,EmptyHeader,EmptyMedia,EmptyTitle,} from "@/components/ui/empty"
 import { Alert, AlertTitle, AlertDescription } from "@/components/ui/alert";
 import { Button } from "@/components/ui/button";
-import { Skeleton } from "@/components/ui/skeleton"
+import { Progress } from "@/components/ui/progress"
 
 export default function Home() {
+  const tempsAvantQuestion = 3000;
+  const espaceDébut = 10;
   const [questions, setQuestions] = useState<any[]>([]);
   const [questionIndex, setQuestionIndex] = useState(0);
   const [explication, setExplication] = useState("");
   const [afficherExplication, setAfficherExplication] = useState(false);
+  const [boutonProchaineQuestion, setboutonProchaineQuestion] = useState(true);
+  const [réponseDonné, setréponseDonné] = useState(-1);
+  const [questionProgress, setquestionProgress] = useState(espaceDébut);
 
   const question = questions[questionIndex];
   
@@ -27,40 +32,60 @@ export default function Home() {
         .select(` id, texte, image_url, image_credit_nom, image_credit_url,explication, reponses:reponse (id,texte,est_correcte)`)
         .order('id', { ascending: true });
       if (error) console.error(error);
-      else setQuestions(data || []);; // On stocke la première question dans l’état
+      else setQuestions(data || []);;
     }
 
     fetchQuestion();
   }, []);
 
-  function reloadQuestions() {
+  function ProchaineQuestion() {
+    setAfficherExplication(false);
+    setboutonProchaineQuestion(true);
+    setExplication("");
+    setQuestionIndex((prev) => prev + 1);
+    setquestionProgress((100-espaceDébut)-((questionIndex) / questions.length) * (100/questions.length - espaceDébut));
+  }
+
+  function reloadQuestion() {
     setQuestionIndex(0);
+    setquestionProgress(espaceDébut);
   }
 
   function handleClick(reponse: any) {
     if (!question || afficherExplication) return;
     const estBonneReponse = reponse.est_correcte;
+    setréponseDonné(reponse.id);
     const message = estBonneReponse
       ? "Bonne réponse !"
       : "Mauvaise réponse.";
     const explicationTexte = question.explication || message;
     setExplication(explicationTexte);
     setAfficherExplication(true);
+    if (estBonneReponse) {
+      setboutonProchaineQuestion(false);
+    }else {
     setTimeout(() => {
-      setAfficherExplication(false);
-      setExplication("");
-      setQuestionIndex((prev) => prev + 1);
-    }, 3000);
+      setboutonProchaineQuestion(false);
+    }, tempsAvantQuestion);
+    }
+  }
+
+  function classRéponse(reponse: any, afficherExplication: any, id: any) {
+    if (afficherExplication && id === réponseDonné) {
+      if (reponse.est_correcte) return "w-full justify-start mt-2 bg-green-100  border-green-400 text-green-900";
+      else                      return "w-full justify-start mt-2 bg-red-100    border-red-400   text-red-900";
+    }
+    return "w-full justify-start mt-2";
   }
 
   if (!question && questions.length > 0) {
     return (
-      <Card className="max-w-4xl mx-auto mt-25">
+      <Card className="max-w-2xl mx-auto mt-75 p-6">
         <div className="text-center mt-auto">
           <h2 className="text-2xl font-bold">Quiz terminé !</h2>
           <p className="mt-4 text-muted-foreground">Merci d’avoir participé.</p>
           <Button
-            onClick={reloadQuestions}
+            onClick={reloadQuestion}
             className="mt-6"
           >
             Recommencer le quiz
@@ -72,16 +97,8 @@ export default function Home() {
 
   return (
     <div>
-      <Alert className="bg-blue-50 border-blue-300 text-blue-800 max-w-xl mx-auto mt-6">
-        <AlertTitle className="text-xl font-semibold">Bienvenue sur CyberQuiz</AlertTitle>
-        <AlertDescription>
-          Préparez-vous à tester vos connaissances !
-        </AlertDescription>
-      </Alert>
-
       {question ? (
-
-        <Card className="max-w-4xl mx-auto mt-6">
+        <Card className="max-w-4xl mx-auto mt-45">
           <div className="flex">
             {/* Colonne gauche : image + crédit */}
             <div className="w-1/2 p-4">
@@ -120,7 +137,8 @@ export default function Home() {
             {/* Colonne droite : question + réponses */}
             <div className="w-1/2 p-4">
               <CardHeader className="p-0 mb-4">
-                <CardTitle>Question {questionIndex + 1} sur {questions.length}</CardTitle>
+                <CardTitle className="mx-auto" >Question {questionIndex + 1} sur {questions.length}</CardTitle>
+                <Progress value={questionProgress} className="w-[90%] mx-auto" />
               </CardHeader>
               <CardContent className="p-0">
                 <p className="text-lg font-semibold mb-4">{question.texte}</p>
@@ -129,7 +147,7 @@ export default function Home() {
                     key={reponse.id}
                     onClick={() => handleClick(reponse)}
                     disabled={afficherExplication}
-                    className="w-full justify-start mt-2"
+                    className={classRéponse(reponse, afficherExplication, reponse.id)}
                     variant="outline"
                   >
                     {reponse.texte}
@@ -142,25 +160,39 @@ export default function Home() {
                 <AlertDescription>{explication}</AlertDescription>
               </Alert>
               )}
+              <div className="text-center mt-6">
+                <Button onClick={ProchaineQuestion} hidden={boutonProchaineQuestion}>
+                  Prochaine question
+                </Button>
+              </div>
             </div>
           </div>
         </Card>
 
       ) : (
 
-        <Card className="max-w-4xl mx-auto mt-6">
-          <Empty className="w-full">
-            <EmptyHeader>
-              <EmptyMedia variant="icon">
-                <Spinner />
-              </EmptyMedia>
-              <EmptyTitle>Chargement de la question...</EmptyTitle>
-              <EmptyDescription>
-                Le Chargement peut durer un certain temps.
-              </EmptyDescription>
-            </EmptyHeader>
-          </Empty>
-        </Card>
+        <div>
+          <Alert className="bg-blue-50 border-blue-300 text-blue-800 max-w-xl mx-auto mt-50">
+            <AlertTitle className="text-xl font-semibold">Bienvenue sur CyberQuiz</AlertTitle>
+            <AlertDescription>
+              Préparez-vous à tester vos connaissances !
+            </AlertDescription>
+          </Alert>
+
+          <Card className="max-w-4xl mx-auto mt-6">
+            <Empty className="w-full">
+              <EmptyHeader>
+                <EmptyMedia variant="icon">
+                  <Spinner />
+                </EmptyMedia>
+                <EmptyTitle>Chargement de la question...</EmptyTitle>
+                <EmptyDescription>
+                  Le Chargement peut durer un certain temps.
+                </EmptyDescription>
+              </EmptyHeader>
+            </Empty>
+          </Card>
+        </div>
 
       )}
     </div>
