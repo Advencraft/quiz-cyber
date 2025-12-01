5d107dae8cd410ba68b9f4f9c367bee8937491dd
MARIE Théo <theomarie50350@gmail.com>
2025-12-01 01:56:04 +0100

    Ajout du classement des joueurs

--- app/page.tsx
+++ app/page.tsx
@@ -15,6 +15,7 @@ import { Switch } from "@/components/ui/switch"
 import { Label } from "@/components/ui/label"
 import { NavigationMenu, NavigationMenuList, NavigationMenuItem, navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
 import { NavigationMenuLink } from '@radix-ui/react-navigation-menu';
+import { Table, TableBody, TableCaption, TableCell, TableFooter, TableHead, TableHeader, TableRow} from "@/components/ui/table"
   
 export default function Home() {
   // --------------------------------------------------------- various variables --------------------------------------------------------- //
@@ -32,6 +33,49 @@ export default function Home() {
     if (page === 'questionnaire') reloadQuestion();
     setPages(page);
   }
+
+  function chargement(section: string) {
+    return (
+      <section id="chargement-section">
+        <Card className="max-w-4xl mx-auto mt-10">
+          <Empty className="w-full">
+            <EmptyHeader>
+              <EmptyMedia variant="icon">
+                <Spinner />
+              </EmptyMedia>
+              <EmptyTitle>Chargement du {section}...</EmptyTitle>
+              <EmptyDescription>
+                Le Chargement peut durer un certain temps.
+              </EmptyDescription>
+            </EmptyHeader>
+          </Empty>
+        </Card>
+      </section>
+    );
+  }
+
+  // ------------------------------------------------------ Questionnaire variables ------------------------------------------------------ //
+  const [Classement, setClassement] = useState<any[]>([]);
+  const [ClassementnIndex, setClassementnIndex] = useState(0);
+  const ClassementJoueur = JSON.stringify(Classement[ClassementnIndex]);
+
+  // --------------------------------------------------- Questionnaire functionality ----------------------------------------------------- //
+  useEffect(() => {
+    async function fetchClassement() {
+      const { data, error } = await supabase
+        .from('classement_joueur')
+        .select(` id, classements:classement (id, date_partie, score, temps), classements_joueurs:joueur (id, pseudo)`)
+        .order('id', { ascending: true });
+      if (error) console.error(error);
+      else setClassement(data || []);;
+    }
+
+    fetchClassement();
+  }, []);
+
+  console.log('ClassementJoueur : '+ClassementJoueur);
+  console.log('Classement : '+JSON.stringify(Classement));
+  
 
   // ------------------------------------------------------ Questionnaire variables ------------------------------------------------------ //
   const tempsAvantQuestion = 3000;
@@ -59,6 +103,7 @@ export default function Home() {
 
     fetchQuestion();
   }, []);
+
 
   function ProchaineQuestion() {
     setAfficherExplication(false);
@@ -222,23 +267,45 @@ export default function Home() {
             </Card>
 
           ) : (
-            
-            <Card className="max-w-4xl mx-auto mt-10">
-              <Empty className="w-full">
-                <EmptyHeader>
-                  <EmptyMedia variant="icon">
-                    <Spinner />
-                  </EmptyMedia>
-                  <EmptyTitle>Chargement de la question...</EmptyTitle>
-                  <EmptyDescription>
-                    Le Chargement peut durer un certain temps.
-                  </EmptyDescription>
-                </EmptyHeader>
-              </Empty>
-            </Card>
+          chargement('question')
           )}
         </section>
-      ):( null )}
+      ) : pages === 'classement' ?( 
+        Classement ? (
+          <section id="classement-section">
+            <Card className="max-w-4xl mx-auto mt-10">
+              <div className="text-center mt-auto">
+                <h2 className="text-2xl font-bold">Classement des Joueurs</h2>
+                <p className="mt-4 text-muted-foreground">Voici le classement des joueurs :</p>
+                <pre className="mt-6 text-left">
+                  <Table>
+                    <TableHeader>
+                      <TableRow>
+                        <TableHead className="w-[100px]">Joueur</TableHead>
+                        <TableHead className="w-[100px]">Score</TableHead>
+                        <TableHead className="w-[100px]">Temps</TableHead>
+                        <TableHead className="w-[100px]">Date de la Partie</TableHead>
+                      </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                      {Classement.map((entry) => (
+                        <TableRow key={entry.id}>
+                          <TableCell >{entry.classements_joueurs.pseudo}</TableCell >
+                          <TableCell >{entry.classements.score}</TableCell >
+                          <TableCell >{entry.classements.temps}</TableCell >
+                          <TableCell >{new Date(entry.classements.date_partie).toLocaleDateString()}</TableCell >
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </pre>
+              </div>
+            </Card>
+          </section>
+        ) : (
+          chargement('classement')
+        )
+       ): null }
     </body>
   );
 }
